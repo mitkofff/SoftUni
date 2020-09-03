3431469a805e74ada4cac76419b6d40f5132ab38
mitkofff <mitkofff1@gmail.com>
2020-09-03 18:48:35 +0300

    addTryCatch

--- C#/OOP/Encapsulation/AnimalFarm/Program.cs
+++ C#/OOP/Encapsulation/AnimalFarm/Program.cs
@@ -2,19 +2,29 @@
 {
     using System;
     using AnimalFarm.Models;
+
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             string name = Console.ReadLine();
             int age = int.Parse(Console.ReadLine());
 
-            Chicken chicken = new Chicken(name, age);
-            Console.WriteLine(
-                "Chicken {0} (age {1}) can produce {2} eggs per day.",
-                chicken.Name,
-                chicken.Age,
-                chicken.ProductPerDay);
+            try
+            {
+                Chicken chicken = new Chicken(name, age);
+                Console.WriteLine(
+                    "Chicken {0} (age {1}) can produce {2} eggs per day.",
+                    chicken.Name,
+                    chicken.Age,
+                    chicken.ProductPerDay);
+            }
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
         }
     }
 }
