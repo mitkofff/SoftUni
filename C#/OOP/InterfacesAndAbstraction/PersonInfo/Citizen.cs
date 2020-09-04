c6cbd5ae969a0f007e493af82afe7bd203534bda
mitkofff <mitkofff1@gmail.com>
2020-09-05 00:14:57 +0300

    personInfo

---
+++ C#/OOP/InterfacesAndAbstraction/PersonInfo/Citizen.cs
@@ -0,0 +1,40 @@
+﻿namespace PersonInfo
+{
+using System;
+    public class Citizen : IPerson
+    {
+        private string name;
+        private int age;
+
+        public Citizen(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name
+        {
+            get => this.name;
+            set
+            {
+                if(value.Length<2 || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("The name should be longer than one character");
+                }
+                this.name = value;
+            }
+        }
+        public int Age
+        {
+            get => this.age;
+            set
+            {
+                if(value<0)
+                {
+                    throw new ArgumentException("Age can't be negative number.");
+                }
+                this.age = value;
+            }
+        }
+    }
+}
---
+++ C#/OOP/InterfacesAndAbstraction/PersonInfo/IPerson.cs
@@ -0,0 +1,9 @@
+﻿namespace PersonInfo
+{
+    interface IPerson
+    {
+        string Name { get; set;}
+        int Age { get; set; }
+
+    }
+}
