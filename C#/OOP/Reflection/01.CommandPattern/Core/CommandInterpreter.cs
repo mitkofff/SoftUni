7d9f432713008253395b84a868d07d1b8688ede3
mitkofff <mitkofff1@gmail.com>
2020-09-12 16:45:38 +0300

    Reflection and Attributes

---
+++ C#/OOP/Reflection/01.CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,37 @@
+﻿namespace CommandPattern.Core
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using CommandPattern.Core.Contracts;
+    public class CommandInterpreter : ICommandInterpreter
+    {
+            private const string CommandSufix = "Command";
+        public string Read(string args)
+        {
+
+            string[] commandTokens = args
+                .Split(' ', System.StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            string commandName = commandTokens[0] + CommandSufix;
+            string[] commandArgs = commandTokens
+                .Skip(1)
+                .ToArray();
+
+            Assembly assembly = Assembly.GetCallingAssembly();
+            Type commandType = assembly
+                .GetTypes()
+                .FirstOrDefault(t => t.Name.ToLower() == commandName.ToLower());
+
+            if(commandType == null)
+            {
+                throw new ArgumentException("Invalid command type!");
+            }
+            ICommand commandInstance = (ICommand)Activator.CreateInstance(commandType);
+
+            string result = commandInstance.Execute(commandArgs);
+            return result;
+        }
+    }
+}
---
+++ C#/OOP/Reflection/01.CommandPattern/Models/Commands/ExitCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace CommandPattern.Models.Commands
+{
+    using System;
+    using CommandPattern.Core.Contracts;
+    public class ExitCommand : ICommand
+    {
+        public string Execute(string[] args)
+        {
+            Environment.Exit(0);
+            return null;
+        }
+    }
+}
