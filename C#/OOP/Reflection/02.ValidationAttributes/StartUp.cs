171811718ffbfb9be4abead00cb0098229aa1ec0
mitkofff <mitkofff1@gmail.com>
2020-09-14 23:51:35 +0300

    Refflection and Attributes

---
+++ C#/OOP/Reflection/02.ValidationAttributes/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ValidationAttributes.Utilitis;
+using ValidationAttributes.Models;
+
+namespace ValidationAttributes
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var person = new Person
+             (
+                 "Mitko Pav",
+                 20
+             );
+
+            bool isValidEntity = Validator.IsValid(person);
+
+            Console.WriteLine(isValidEntity);
+        }
+    }
+}
---
+++ C#/OOP/Reflection/02.ValidationAttributes/Utilitis/Validator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace ValidationAttributes.Utilitis
+{
+    using System.Linq;
+    using ValidationAttributes.Attributes;
+    public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            if(obj == null)
+            {
+                return false;
+            }
+
+            Type objType = obj.GetType();
+            PropertyInfo[] properties = objType.GetProperties();
+
+            foreach(var property in properties)
+            {
+                MyValidationAttribute[] attributes = property
+                    .GetCustomAttributes()
+                    .Where(ca => ca is MyValidationAttribute)
+                    .Cast<MyValidationAttribute>()
+                    .ToArray();
+
+                foreach(MyValidationAttribute attribute in attributes)
+                {
+                    if(!attribute.IsValid(property.GetValue(obj)))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
