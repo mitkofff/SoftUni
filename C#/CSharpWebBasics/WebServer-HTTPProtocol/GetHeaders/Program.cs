008120079d86781c0847621465025c8fad6caa32
mitkofff <mitkofff1@gmail.com>
2020-09-23 00:54:23 +0300

    WebServer-HTTPProtocol

---
+++ C#/CSharpWebBasics/WebServer-HTTPProtocol/GetHeaders/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpClientDemo3
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.Write("Enter url: ");
+            string url = Console.ReadLine();
+            await GetHeaders(url);
+        }
+
+        private static async Task GetHeaders(string url)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            HttpClient httpClient = new HttpClient();
+
+            httpClient.DefaultRequestHeaders.Add("X-Mit", "mit");
+            string html = await httpClient.GetStringAsync(url);
+            var html2 = await httpClient.GetAsync(url);
+            Console.WriteLine(string.Join(Environment.NewLine,
+                               html2.Headers.Select(x => x.Key + " : " + x.Value)));
+        }
+    }
+}
---
+++ C#/CSharpWebBasics/WebServer-HTTPProtocol/SimpleHttpServer/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace SimpleHttpServer
+{
+    using System;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Text;
+    using System.Threading.Tasks;
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            const string NewLine = "\n\r";
+            TcpListener tcpListener = new TcpListener(IPAddress.Loopback, 1984);
+            tcpListener.Start();
+
+            while (true)
+            {
+                var client = tcpListener.AcceptTcpClient();
+                var stream = client.GetStream();
+
+                byte[] buffer = new byte[100000];
+                var length = stream.Read(buffer, 0, buffer.Length);
+
+                string requestString = Encoding.UTF8.GetString(buffer, 0, length);
+                Console.WriteLine(requestString);
+
+                string html = $"<h1> Hello, I am MitkofffServer! <h1>" +
+                    $"<h2>{DateTime.Now}<h2>"+
+                    $"<form method=post><input name=username /><input name=password />"+
+                    $"<input type=submit /></form>";
+
+                string response = "HTTP/1.1 200 OK" + NewLine
+                    + "Server: MitkofffServer" + NewLine
+                    + "Content-Type: text/html; charset=utf-8" + NewLine
+                    + "Content-Lenght: " + html.Length + NewLine
+                    + NewLine
+                    + html + NewLine;
+
+                byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+                stream.Write(responseBytes);
+
+                Console.WriteLine(new string('*', 80));
+            }
+            
+        }
+    }
+}
